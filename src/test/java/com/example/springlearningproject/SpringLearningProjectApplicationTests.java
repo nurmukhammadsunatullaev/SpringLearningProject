a9a2198a87df609f525da2638e91f0ce4b1b9add
Nurmukhammad_Sunatul <nurmukhammad.sunatullaev@gmail.com>
2022-04-12 15:49:17 +0500

    Project Created

---
+++ src/test/java/com/example/springlearningproject/SpringLearningProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springlearningproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLearningProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
